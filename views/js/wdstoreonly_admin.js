cf12e7f3ba4a44b867aa90b13851b7806e9c95b6
Jeremy <j.leclercq@webdigit.be>
2016-10-06 16:20:52 +0200

    Modification des fichiers js + ajout _admin.js

---
+++ views/js/wdstoreonly_admin.js
@@ -0,0 +1,11 @@
+$(function(){
+	/*console.log('test');
+	console.log($('#storeOnly').val());
+	console.log($('#noStoreOnly').val());
+	console.log($('#submitChoix').val());
+	$('#submitChoix').on('click', function(){
+		event.preventDefault();
+		var checked = $('.choix:checked').val();
+		console.log(checked);
+	});*/
+});
