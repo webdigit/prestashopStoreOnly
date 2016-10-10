f4d16889226132e502d1fdd61dec9f74d930a51a
Jeremy <j.leclercq@webdigit.be>
2016-10-10 16:30:29 +0200

    Suppression des console.log

--- views/js/wdstoreonly_admin.js
+++ views/js/wdstoreonly_admin.js
@@ -1,11 +1,3 @@
 $(function(){
-	/*console.log('test');
-	console.log($('#storeOnly').val());
-	console.log($('#noStoreOnly').val());
-	console.log($('#submitChoix').val());
-	$('#submitChoix').on('click', function(){
-		event.preventDefault();
-		var checked = $('.choix:checked').val();
-		console.log(checked);
-	});*/
+	
 });
